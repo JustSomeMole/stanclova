46be9f1cd97b9375f4a7cb5234ed0ff800acc21c
JustSomeMole <stanclova.terezka@gmail.com>
2024-10-16 15:53:27 +0200

    Funkce Add a Search

--- stanclova_spojove_seznamy/stanclova_spojove_seznamy/Program.cs
+++ stanclova_spojove_seznamy/stanclova_spojove_seznamy/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Node uzlik = new Node(8); //hodnota a hodnota next
+    }
+}
+
+class Node
+{
+    public Note(int value) //konstruktor
+    {
+        Value = value;
+    }
+    public int Value { get; }
+    public Node Next { get; set; }
+}
+
+class LinkedList
+{
+    public Node Head { get; set; }
+
+    public void Add(int value)
+    {
+        if (Head == null) //když je senznam prázdný
+            Head = new Node(value);
+        else
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+    }
+
+    public bool Search(int value)
+    {
+        Node node = Head;
+        while (node.Next != null)
+        {
+            if(node.Value == value) 
+                return true;
+            node = node.Next;
+        }
+        return false;
     }
 }
