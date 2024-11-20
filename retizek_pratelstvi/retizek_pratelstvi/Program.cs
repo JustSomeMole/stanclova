c1d59f9b48717edde1b9138b0396de9776bcbe63
JustSomeMole <stanclova.terezka@gmail.com>
2024-11-20 15:30:10 +0100

    Přijetí vstupu

--- retizek_pratelstvi/retizek_pratelstvi/Program.cs
+++ retizek_pratelstvi/retizek_pratelstvi/Program.cs
@@ -10,7 +10,15 @@ namespace retizek_pratelstvi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kolikpak máme lidí k prohledání? ");
+            int PocetLidi = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Nyní je nutné zadat probíhající přátelské vztahy mezi subjekty (příklad zapsání: 1-4 2-3 3-4): ");
+            string Vztahy = Console.ReadLine();
+
+            Console.WriteLine("Mezi kým hledáš řetízek pln přátelství (příklad zapsání: 1 5)? ");
+            string Hledani = Console.ReadLine();
+            
         }
     }
 
