8c1bd66a5f74b7a0c5372b7b9a7fd0be6e72be6a
JustSomeMole <stanclova.terezka@gmail.com>
2024-11-20 15:57:44 +0100

    Dodělání vstupu, začátek předělání na arraz
    pomoc... špatný

--- retizek_pratelstvi/retizek_pratelstvi/Program.cs
+++ retizek_pratelstvi/retizek_pratelstvi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,16 +15,25 @@ namespace retizek_pratelstvi
             int PocetLidi = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Nyní je nutné zadat probíhající přátelské vztahy mezi subjekty (příklad zapsání: 1-4 2-3 3-4): ");
-            string Vztahy = Console.ReadLine();
+            string[] Vztahy = Console.ReadLine().Split();
 
             Console.WriteLine("Mezi kým hledáš řetízek pln přátelství (příklad zapsání: 1 5)? ");
             string Hledani = Console.ReadLine();
             
         }
-    }
+        static void HujakasaVytvorList(string[] VlozenaCislaString) //[1-2, 3-4, 1-3] --> {[1, 2], [3, 4], [1, 3]}
+        {
+            int[] //converttoint32
+        }
+    }  
 
     class Kamarad
     {
-        
+        public Kamarad(int jmeno) //každý uyel bude mít: název
+        {
+            Jmeno = jmeno;
+        }
+        public int Jmeno { get; set; }
+
     }
 }
