2b2245e7bb491bf4fe1c8b6539ae04382251d405
JustSomeMole <stanclova.terezka@gmail.com>
2024-12-03 22:58:08 +0100

    HOTOVO - ZÁVORKY.
    najdu si tě, GitKrakene

--- zasobnik/zasobnik/Program.cs
+++ zasobnik/zasobnik/Program.cs
@@ -12,31 +12,24 @@ namespace zasobnik
         {
             Console.WriteLine("Napište Vaše závorky");
             string zavorkyPomocny = Console.ReadLine(); //už rovnou to je jako list polí... každý znak je jedno pole, můžu k nim přistupovat po indexech
-<<<<<<< HEAD
             Console.WriteLine(SpravneZavorkyOtaznik(zavorkyPomocny));
             Console.ReadLine();
-=======
-            ConsoleWriteLine(SpravneZavorkyOtaznik(zavorkyPomocny));
->>>>>>> origin/main
+
         }
         static bool SpravneZavorkyOtaznik(string seznamZavorek)
         {
             Stack<char> stackZavorek = new Stack<char>(); //char je jeden znak v string  
-<<<<<<< HEAD
             stackZavorek.Push(seznamZavorek[0]);
+
             for (int i = 1; i < seznamZavorek.Length; i++)
             {
-                char prvniVstack = stackZavorek.Peek(); //nutný zkontrlovat, že tam něco je, jinak nejde peek!!!!!       
-=======
-            for (int i = 1; i < seznamZavorek.Length; i++)
-            {
-                
-                char prvniVstack = stackZavorek.Peek(); //nutný zkontrlovat, že tam něco je, jinak nejde peek!!!!!
->>>>>>> origin/main
-                
-                if (prvniVstack == '(' && seznamZavorek[i] == ')' || prvniVstack == '[' && seznamZavorek[i] == ']' || prvniVstack == '{' && seznamZavorek[i] == '}')
+                if (stackZavorek.Count != 0)
                 {
-                    stackZavorek.Pop();
+                    char prvniVstack = stackZavorek.Peek(); //nutný zkontrlovat, že tam něco je, jinak nejde peek!
+                    if (prvniVstack == '(' && seznamZavorek[i] == ')' || prvniVstack == '[' && seznamZavorek[i] == ']' || prvniVstack == '{' && seznamZavorek[i] == '}')
+                    {
+                        stackZavorek.Pop();
+                    }
                 }
 
                 else
@@ -48,7 +41,7 @@ namespace zasobnik
             {
                 return false;
             }
-            return true;     
+            return true;     //gitkraken mi právě smazal půlku mého kódu... 
         }
     }
 }
