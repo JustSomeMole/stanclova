e6bd8978d8b5448dc48179bee63394ebebf43369
JustSomeMole <stanclova.terezka@gmail.com>
2024-12-03 22:42:20 +0100

    Závorky - úkol - HOTOVO
    [  :

--- zasobnik/zasobnik/Program.cs
+++ zasobnik/zasobnik/Program.cs
@@ -12,15 +12,16 @@ namespace zasobnik
         {
             Console.WriteLine("Napište Vaše závorky");
             string zavorkyPomocny = Console.ReadLine(); //už rovnou to je jako list polí... každý znak je jedno pole, můžu k nim přistupovat po indexech
-            ConsoleWriteLine(SpravneZavorkyOtaznik(zavorkyPomocny));
+            Console.WriteLine(SpravneZavorkyOtaznik(zavorkyPomocny));
+            Console.ReadLine();
         }
         static bool SpravneZavorkyOtaznik(string seznamZavorek)
         {
             Stack<char> stackZavorek = new Stack<char>(); //char je jeden znak v string  
+            stackZavorek.Push(seznamZavorek[0]);
             for (int i = 1; i < seznamZavorek.Length; i++)
             {
-                
-                char prvniVstack = stackZavorek.Peek(); //nutný zkontrlovat, že tam něco je, jinak nejde peek!!!!!
+                char prvniVstack = stackZavorek.Peek(); //nutný zkontrlovat, že tam něco je, jinak nejde peek!!!!!       
                 
                 if (prvniVstack == '(' && seznamZavorek[i] == ')' || prvniVstack == '[' && seznamZavorek[i] == ']' || prvniVstack == '{' && seznamZavorek[i] == '}')
                 {
