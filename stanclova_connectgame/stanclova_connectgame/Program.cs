254458ee15b23f94cd2d62c0007338b8615ba05a
JustSomeMole <stanclova.terezka@gmail.com>
2025-04-09 00:41:27 +0200

    CONNECT FOUR
    je nutne opravit renderovani pole.... nefunguje

--- stanclova_connectgame/stanclova_connectgame/Program.cs
+++ stanclova_connectgame/stanclova_connectgame/Program.cs
@@ -1,55 +1,246 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Media;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
+//using namespace;
 
-namespace connectgame
+namespace ConnectGame
 {
-    internal class Program
+    internal class Program  
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            int[,] board = new int[6, 7]{
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 1, 2, 1, 0 },
-                { 2, 2, 2, 2, 2, 1, 0 }
-            };
-            int[] position = { 5, 0 };
-            // pozice řádek s indexem 5
-            // pozice sloupec s indexem 0
-                
-            int hrac = 2;
-            int pocetKamenuNaVyhru = 5;
-            
+            Console.WriteLine("Vítejte ve hře Propadávacích piškvorek!");
+            Console.WriteLine("Pro spuštění stiskněte libovolnou klávesu.");
 
-            Console.WriteLine(Check(board, position, hrac, pocetKamenuNaVyhru));
+            Console.ReadKey(); //počká na stisknutí jakýkoliv klávesnice, pak to vymažu
+
+            Console.Clear();   
+
+            Console.WriteLine("Zvolte si libovolnou šířku Vašeho pole: ");
+            int sirkaPole = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zvolte si libovolnou výšku Vašeho pole: ");
+            int vyskaPole = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zvolte si kolik bude hráč muset spojit žetonů, aby vyhrál: ");
+            int pocetVyhernichZetonu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zvolte si kolik hráčů bude hrát (možnosti: 2 až 4 hráři):");
+            int pocetHracu = int.Parse(Console.ReadLine());
+
+
+
+            Hra hra = new Hra(pocetVyhernichZetonu, sirkaPole, vyskaPole, pocetHracu);
+            hra.Play();
+
+            Console.WriteLine("Díky za hraní Propadávacích piškvorek, tak příště!"); //dokud poběží Play, tak se to nenapíše, pak jo
             Console.ReadLine();
-
         }
+    }
 
-        static bool Check(int[,] board, int[] soucasnaPozice, int hrac, int pocetKamenuNaVyhru)
+
+
+
+
+    //-------PRIVÁTNÍ (kromě Play, .... myslím)-------
+
+
+
+
+
+    class Hra
+    {
+        private int[,] hraciPole; //pro tuhle classu inicializuju tuto proměnnou
+        private Hrac[] hraci;
+        private int hracNaTahuIndex;
+        private int pocetVyhernichZetonu;
+        private int SoucetTahu;
+        private int maxTahu;
+
+        public Hra(int pocetVyhernichZetonu, int sirkaPole, int vyskaPole, int pocetHracu) //konstruktor
         {
-           int radek = soucasnaPozice[0];
-           int sloupec = soucasnaPozice[1];
-           return CheckColumn(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckRow(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckDiag(board, radek, sloupec, hrac, pocetKamenuNaVyhru);
+            this.pocetVyhernichZetonu = pocetVyhernichZetonu;
+            hraciPole = new int[sirkaPole, vyskaPole];
+            hraci = new Hrac[pocetHracu];
+            maxTahu = vyskaPole * sirkaPole; //max, pak zaplněno
+            SoucetTahu = 0;
+            hracNaTahuIndex = 0;
+
+            NainicalizujHrace();
+        }
+        //nastavím a připravím hráče
+        private void NainicalizujHrace()
+        {
+            string[] symboly = { "X", "O", "#", "@" };
+            for (int i = 0; i < hraci.Length; i++)
+            {
+                hraci[i] = new Hrac($"Hráč {i + 1}", symboly[i]);
+            }
         }
 
-        static bool CheckColumn(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+
+        //-------ZAPNUTÍ HRY-------
+
+
+        public void Play()
+        {
+            bool konecHry = false;
+            Hrac vyhral = null;
+
+            while (konecHry != true)
+            {
+                ZobrazPole();
+
+
+                Hrac hracNaTahu = hraci[hracNaTahuIndex]; //změním hráče na hráče na tahu
+
+                Console.WriteLine($"{hracNaTahu.Jmeno}, zadejte sloupec, do kterého chcete dát žeton: ");
+
+                int sloupec = ZadejPlatnySloupec();
+                int radek = VhozeniZetonu(sloupec - 1, hracNaTahuIndex + 1); //na jaký řádek žeton spadl
+
+                int[] soucasnaPozice = new int[] { radek, sloupec - 1 };
+
+                if (Check(hraciPole, new int[] { radek, sloupec - 1 }, hracNaTahuIndex + 1, pocetVyhernichZetonu))
+                {
+                    konecHry = true;
+                    vyhral = hracNaTahu;
+                }
+                else if (++SoucetTahu >= maxTahu)
+                {
+                    konecHry = true;
+                }
+                else
+                {
+                    hracNaTahuIndex = (hracNaTahuIndex + 1) % hraci.Length;
+                }
+
+            }
+            ZobrazPole();
+            if (vyhral != null)
+            {
+                Console.WriteLine($"Gratulujeme! {vyhral.Jmeno} vyhrál!");
+            }
+            else
+            {
+                Console.WriteLine("Remíza! Hrací pole je plné.");
+            }
+
+        }
+
+
+        //-------POMOCNÉ FUNKCE-------
+
+        private int ZadejPlatnySloupec()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int sloupec)) //funkce se pokusí předělat text na celé číslo a když se podaří tak h uloží ho do sloupce
+                {
+                    if (sloupec >= 1 && sloupec <= hraciPole.GetLength(1)) //jsme v sloupcích od 1 do max poctu/okraj
+                    {
+                        if (hraciPole[0, sloupec - 1] == 0) //uživatel dává od 1, pole indexováno od 0
+                                                            //jestli tam nic není
+                        {
+                            return sloupec; //vrátím ho protože je ok
+                        }
+                    }
+                    Console.WriteLine("Sloupec už je plný.");
+                }
+                else
+                {
+                    Console.WriteLine("Neplatný vstup.");
+                }
+            }
+        }
+
+
+        private int VhozeniZetonu(int sloupec, int hracID) 
+        {                                                                     //getlenght(0) mi vrátí pocet rádku, takže výsku
+            for (int radek = hraciPole.GetLength(0) - 1; radek >= 0; radek--) //začnu odspodu - musím odečíst 1, abych měla řádek
+                                                                              //jdu nahoru, odspodu - odečítám
+            {
+                if (hraciPole[radek, sloupec] == 0)
+                {
+                    hraciPole[radek, sloupec] = hracID;
+                    return radek;
+                }
+            }
+            return -1; //mělo by se vždy podařit, protože jsem checkovala jestli tam něco není a jde to vhodit
+
+        }
+
+
+        private void ZobrazPole()
+        {
+            Console.Clear();
+
+            //vypíšu si číslíčka nad sloupci
+            for (int i = 0; i < hraciPole.GetLength(1); i++) //getlenght funguje ve vícerozměrných polích
+                                                             //... třeba to co mám, dá mi zpět délku sloupce
+                                                             //... (0) by byly řádky
+            {
+                Console.Write($" {i + 1} ");
+            }
+            Console.WriteLine();
+
+            //projdu řádky
+            for (int i = 0; i < hraciPole.GetLength(0); i++)                        
+            {                                                                       
+                Console.Write("|");
+                //peojdu sloupce                                                    
+                for (int j = 0; j < hraciPole.GetLength(1); j++)                    
+                {
+                    int pole = hraciPole[i, j];
+
+                    if (pole == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (pole >= 1)
+                    {
+                        Console.Write($"{hraci[pole - 1].Symbol}|");
+                    }
+                }
+                Console.WriteLine();
+            }
+            //dolejšek
+            for (int i = 0; i < hraciPole.GetLength(1); i++)
+            {
+                Console.Write("---");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        //---------CHECK VÝHRY---------
+
+
+        public bool Check(int[,] board, int[] soucasnaPozice, int hrac, int pocetKamenuNaVyhru)
+        {
+            int radek = soucasnaPozice[0];
+            int sloupec = soucasnaPozice[1];
+            return CheckColumn(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckRow(board, radek, sloupec, hrac, pocetKamenuNaVyhru) || CheckDiag(board, radek, sloupec, hrac, pocetKamenuNaVyhru);
+        }
+
+        bool CheckColumn(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
         {
             int pocet = 0;
             int pocetRadku = gameField.GetLength(0);
 
-            for (int i = radek; i < pocetRadku; i++) //protože jsou to padaci piskvorky, tak nepůjdu nahoru proti gravitaci, nelevitují
+            for (int i = radek; i < pocetRadku; i++)
             {
                 if (gameField[i, sloupec] == currentPlayer)
                 {
                     pocet++;
-                    if (pocet >= reqForWin) //kdyby náhodou
+                    if (pocet >= reqForWin)
                     {
                         return true;
                     }
@@ -61,11 +252,10 @@ namespace connectgame
             }
             return false;
         }
-
-        static bool CheckRow(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+        bool CheckRow(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
         {
-            int pocet = 1; //začnu od jedné, protože aktuální kámen bude vždy ten toho hráče, komu to kontrluji
-            int pocetSloupcu = gameField.GetLength(1); 
+            int pocet = 1;
+            int pocetSloupcu = gameField.GetLength(1);
 
             for (int i = sloupec + 1; i < pocetSloupcu; i++)
             {
@@ -83,8 +273,8 @@ namespace connectgame
                 }
             }
 
-            // vlevo vbok!
-            for (int i = sloupec - 1; i >= 0 ; i--) //odečtu tam 1, protože už jsem ji počítala do součtu v prnvím cyklu
+
+            for (int i = sloupec - 1; i >= 0; i--)
             {
                 if (gameField[radek, i] == currentPlayer)
                 {
@@ -102,14 +292,13 @@ namespace connectgame
 
             return false;
         }
-
-        static bool CheckDiag(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
+        bool CheckDiag(int[,] gameField, int radek, int sloupec, int currentPlayer, int reqForWin)
         {
             int pocet = 1;
             int pocetRadku = gameField.GetLength(0);
             int pocetSloupcu = gameField.GetLength(1);
 
-            for (int i = 1; radek + i < pocetRadku && sloupec + i < pocetSloupcu; i++) //diagonála \ dolu; pricitam k radku i sloupci
+            for (int i = 1; radek + i < pocetRadku && sloupec + i < pocetSloupcu; i++)
             {
                 if (gameField[radek + i, sloupec + i] == currentPlayer)
                 {
@@ -125,7 +314,7 @@ namespace connectgame
                 }
             }
 
-            for (int i = 1; radek - i >= 0 && sloupec - i >= 0; i++) //diagonála \ nahoru; odecitam od ksloupce i radku
+            for (int i = 1; radek - i >= 0 && sloupec - i >= 0; i++)
             {
                 if (gameField[radek - i, sloupec - i] == currentPlayer)
                 {
@@ -142,10 +331,10 @@ namespace connectgame
             }
 
 
-            pocet = 1; //resetování počtu abych si to neměnila pri pruchodu jednou a pak druhou - jsou separatni 
+            pocet = 1;
 
 
-            for (int i = 1; radek - i >= 0 && sloupec + i < pocetSloupcu; i++) //diagonála / nahoru; odecitam od radku, pricitam k sloupci
+            for (int i = 1; radek - i >= 0 && sloupec + i < pocetSloupcu; i++)
             {
                 if (gameField[radek - i, sloupec + i] == currentPlayer)
                 {
@@ -161,7 +350,7 @@ namespace connectgame
                 }
             }
 
-            for (int i = 1; radek + i < pocetRadku && sloupec - i >= 0; i++) //diagonála / dolu; pricitam k radku, odectiam od sloupce
+            for (int i = 1; radek + i < pocetRadku && sloupec - i >= 0; i++)
             {
                 if (gameField[radek + i, sloupec - i] == currentPlayer)
                 {
@@ -178,6 +367,23 @@ namespace connectgame
             }
 
             return false;
+        }
+
+    }
+
+
+    //--------JINÉ TRÍDY--------
+
+
+    class Hrac
+    {
+        public string Jmeno { get; } //public abz to bylo vidět
+        public string Symbol { get; }
+
+        public Hrac(string jmeno, string symbol)
+        {
+            Jmeno = jmeno;
+            Symbol = symbol;
         }
     }
 }
